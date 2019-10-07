d974a812eee7fab85e61bfa58ed31a755158289e
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-07 16:34:57 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,6 @@
 def unsafe?(speed)
-if speed >= 60
-  return true
-  elsif speed <= 40
-  return true
-else
-  return false
-
+speed > 60 ? true : false
+speed < 40 ? true : false
 end
 
 def not_safe?(speed)
